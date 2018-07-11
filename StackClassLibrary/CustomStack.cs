7a6e07fe17bbb9bc887c0eb3d5cbc152750602ff
taler0n <leonchik.vin@mail.ru>
2018-07-11 22:40:50 +0300

    Добавьте файлы проекта.

---
+++ StackClassLibrary/CustomStack.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace StackClassLibrary
+{
+    public class CustomStack<T>
+    {
+        class Node
+        {
+            public T _value;
+            public Node _next;
+            public Node(T value)
+            {
+                _value = value;
+            }
+        }
+
+        Node _top;
+        public int Count { get; private set; }
+        public CustomStack()
+        {
+            _top = null;
+            Count = 0;
+        }
+        public void Push(T value)
+        {
+            var newNode = new Node(value);
+            newNode._next = _top;
+            _top = newNode;
+            Count++;
+        }
+        public T Peek()
+        {
+            if (_top != null)
+                return _top._value;
+            throw new InvalidOperationException("Stack is empty");
+        }
+        public T Pop()
+        {
+            if (_top != null)
+            {
+                T tmp = _top._value;
+                _top = _top._next;
+                Count--;
+                return tmp;
+            }
+            throw new InvalidOperationException("Stack is empty");
+        }
+        public void Clear()
+        {
+            _top = null;
+            Count = 0;
+        }
+        public bool Contains(T value)
+        {
+            bool found = false;
+            var tmpNode = _top;
+            while (tmpNode != null)
+            {
+                if (tmpNode._value.Equals(value))
+                {
+                    found = true;
+                    break;
+                }
+                tmpNode = tmpNode._next;
+            }
+            return found;
+        }
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            var tmpNode = _top;
+            while (tmpNode != null)
+            {
+                sb.AppendFormat("{0} ", tmpNode._value);
+                tmpNode = tmpNode._next;
+            }
+            return sb.ToString();
+        }
+        public T[] ToArray()
+        {
+            if (Count > 0)
+            {
+                T[] array = new T[Count];
+                var tmpNode = _top;
+                for (int i = 0; i < Count; i++)
+                {
+                    array[i] = tmpNode._value;
+                    tmpNode = tmpNode._next;
+                }
+                return array;
+            }
+            throw new InvalidOperationException("Stack is empty");
+        }
+    }
+}
---
+++ StackUnitTestProject/StackTests.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using StackClassLibrary;
+
+namespace StackUnitTestProject
+{
+    [TestClass]
+    public class StackTests
+    {
+        [TestMethod]
+        public void BasicPushPopTest()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            Assert.AreEqual(2, stack.Pop());
+            Assert.AreEqual(1, stack.Pop());
+        }
+
+        [TestMethod]
+        public void PushIncreasesCount()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            Assert.AreEqual(2, stack.Count);
+        }
+
+        [TestMethod]
+        public void PopDecreasesCount()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Pop();
+            Assert.AreEqual(1, stack.Count);
+        }
+
+        [TestMethod]
+        public void BasicPeekTest()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            Assert.AreEqual(2, stack.Peek());
+        }
+
+        [TestMethod]
+        public void BasicClearTest()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Clear();
+            try
+            {
+                stack.Pop();
+                Assert.Fail();
+            }
+            catch (Exception e)
+            {
+                if (!(e is InvalidOperationException))
+                    Assert.Fail();
+            }
+        }
+
+        [TestMethod]
+        public void BasicContainsTest()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            Assert.AreEqual(true, stack.Contains(1));
+            Assert.AreEqual(false, stack.Contains(6));
+        }
+
+        [TestMethod]
+        public void BasicToStringTest()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            Assert.AreEqual("2 1 ", stack.ToString());
+        }
+
+        [TestMethod]
+        public void BasicToArrayTest()
+        {
+            CustomStack<int> stack = new CustomStack<int>();
+            stack.Push(1);
+            stack.Push(2);
+            var arr = stack.ToArray();
+            Assert.AreEqual(2, arr[0]);
+            Assert.AreEqual(1, arr[1]);
+        }
+    }
+}
